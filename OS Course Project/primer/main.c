9bdd55bdff0cc58292d1a020ed5522ab964ab95f
Tihomir13 <tihomir13n2002@abv.bg>
2024-10-27 22:38:42 +0200

    update upr 2 os project

---
+++ OS Course Project/primer/main.c
@@ -0,0 +1,17 @@
+#define FIBER_STACK 1024 * 64
+ucontext_t uctx_func, uctx_main;
+void func1()
+{
+    printf(“Hello I am func1\n”);
+}
+int main()
+{
+    getcontext(&uctx_func);
+    uctx_func.uc_link = NULL;
+    uctx_func.uc_stack.ss_sp = malloc(FIBER_STACK);
+    uctx_func.uc_stack.ss_size = FIBER_STACK;
+    uctx_func.uc_stack.ss_flags = 0;
+    makecontext(&uctx_func, func1, 0);
+    swapcontext(&uctx_main, &uctx_func);
+    ...
+}
---
+++ OS Course Project/primer/primer1.c
@@ -0,0 +1,14 @@
+#include <malloc.h>
+#include <ucontext.h>
+#include <stdio.h>
+#define FIBER_STACK 1024 * 64
+ucontext_t uctx_func, uctx_main;
+void fiber()
+{
+    printf(“Fiber : started\n”);
+    printf(“Fiber : Now swap context to main\n”);
+    swapcontext(&uctx_func, &uctx_main);
+    printf(“Fiber : returned\n”);
+    printf(“Fiber : Now swap context again to main\n”);
+    swapcontext(&uctx_func, &uctx_main);
+}
