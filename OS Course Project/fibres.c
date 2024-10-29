55b4975571a7a1e5da0fa0270fc327d4b24e4362
Tihomir13 <tihomir13n2002@abv.bg>
2024-10-29 23:10:51 +0200

    update fibres

--- OS Course Project/fibres.c
+++ OS Course Project/fibres.c
@@ -5,54 +5,30 @@
 #define FIBER_STACK 1024 * 64
 ucontext_t mainContext, fiber1Context, fiber2Context, fiber3Context;
 
-void fiber1() {
-    printf("fiber1 start\n");
-    swapcontext(&fiber1Context, &fiber2Context);
-    printf("fiber1 end\n");
-    swapcontext(&fiber1Context, &fiber3Context);
+void fiber(ucontext_t &currContext, ucontext_t &nextContext, const char *nameCurr, const char *nameNext)
+{
+    printf("%s: started\n", nameCurr);
+    printf("%s: Now swap context to %s\n", nameCurr, nameNext);
+    swapcontext(&currContext, &nextContext);
+    printf("%s: finished\n", nameCurr);
 }
 
-void fiber2() {
-    printf("fiber2 start\n");
-    swapcontext(&fiber2Context, &fiber3Context);
-    printf("fiber2 end\n");
-    swapcontext(&fiber2Context, &fiber1Context);
-}
-
-void fiber3() {
-    printf("fiber3 start\n");
-    swapcontext(&fiber3Context, &fiber1Context);
-    printf("fiber3 end\n");
-    swapcontext(&fiber3Context, &mainContext);
-}
-
-int main() {
-    char stack1[FIBER_STACK], stack2[FIBER_STACK], stack3[FIBER_STACK];
+int main()
+{
+    char stack1[FIBER_STACK];
 
     // Инициализация на fiber1
     getcontext(&fiber1Context);
     fiber1Context.uc_stack.ss_sp = stack1;
     fiber1Context.uc_stack.ss_size = sizeof(stack1);
-    fiber1Context.uc_link = &mainContext;
-    makecontext(&fiber1Context, fiber1, 0);
+    fiber1Context.uc_link = &mainContext; // Връща се към mainContext след приключване на fiber1
+    makecontext(&fiber1Context, (void (*)())fiber, 4, std::ref(fiber1Context), std::ref(mainContext), "Fiber1", "Main");
 
-    // Инициализация на fiber2
-    getcontext(&fiber2Context);
-    fiber2Context.uc_stack.ss_sp = stack2;
-    fiber2Context.uc_stack.ss_size = sizeof(stack2);
-    fiber2Context.uc_link = &mainContext;
-    makecontext(&fiber2Context, fiber2, 0);
+    printf("Main: swap context to Fiber1\n");
+    swapcontext(&mainContext, &fiber1Context); // Превключване към Fiber1
+    printf("Main: returned from Fiber1\n");
 
-    // Инициализация на fiber3
-    getcontext(&fiber3Context);
-    fiber3Context.uc_stack.ss_sp = stack3;
-    fiber3Context.uc_stack.ss_size = sizeof(stack3);
-    fiber3Context.uc_link = &mainContext;
-    makecontext(&fiber3Context, fiber3, 0);
-
-    printf("main start\n");
-    swapcontext(&mainContext, &fiber1Context);
-    printf("main end\n");
+    printf("Main: exiting\n");
 
     return 0;
 }
