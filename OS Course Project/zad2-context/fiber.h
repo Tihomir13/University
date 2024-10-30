da74e52fb666d37ccf0f91e635eadfde35764df8
Tihomir13 <tihomir13n2002@abv.bg>
2024-10-30 17:46:22 +0200

    update

--- OS Course Project/zad2-context/fiber.h
+++ OS Course Project/zad2-context/fiber.h
@@ -1,9 +1,11 @@
+// fiber.h
 #ifndef FIBER_H
 #define FIBER_H
 
 #include <ucontext.h>
-#include <stdio.h>
+#define FIBER_STACK 1024 * 64
 
+// Декларация на функцията fiber
 void changeContext(ucontext_t* currContext, ucontext_t* nextContext, const char* nameCurr, const char* nameNext);
 
 #endif // FIBER_H
